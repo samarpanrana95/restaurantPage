193426575ada5d46c274d24b6bf477b63b427333
Samarpan Rana <samarpanrana95@gmail.com>
2026-03-18 14:47:16 +0545

    Add Js dyanmic content loading with clickable menu

--- src/index.js
+++ src/index.js
@@ -1,3 +1,64 @@
 import "./styles.css"
+import { todayMenu, tomorrowMenu, overmorrowMenu } from "./menu";
 
-console.log("This works")
+
+class Dom {
+    constructor () {
+        this.setDomEvent()
+    }
+
+    changeFocus (target) {
+        let navs = document.querySelectorAll('.titles > nav')
+        for (let nav of navs) {
+            nav.classList.remove('current')
+        }
+        target.classList.add('current')
+    }
+
+    resetContent () {
+        let content = document.querySelector('.content')
+        let itemDivider = document.querySelector('.item-divider')
+        content.replaceChildren(itemDivider)
+    }
+    
+    createDomItem (itemName, itemPrice) {
+        let item = document.createElement('div')
+        item.classList.add('item')
+        let itemNameDom = document.createElement('div')
+        itemNameDom.classList.add('item-name')
+        itemNameDom.textContent = itemName
+        let itemPriceDom = document.createElement('div')
+        itemPriceDom.classList.add('item-price')
+        itemPriceDom.textContent = '$' + itemPrice
+
+        item.appendChild(itemNameDom)
+        item.appendChild(itemPriceDom)
+        let parent = document.querySelector('.content')
+        parent.appendChild(item)
+    }
+
+    renderMenu(dayMenu) {
+        this.resetContent()
+        for (let key in dayMenu) {
+            this.createDomItem(key, dayMenu[key])
+        }
+    }
+
+    setDomEvent () {
+        let titles = document.querySelector('.titles');
+        titles.addEventListener("click", (e) => {
+            if (e.target.classList.contains('todayMenu')) {
+                this.renderMenu(todayMenu)
+            } 
+            else if (e.target.classList.contains('tomorrowMenu')) {
+                this.renderMenu(tomorrowMenu)
+            }
+            else if (e.target.classList.contains('overmorrowMenu')) {
+                this.renderMenu(overmorrowMenu)
+            }
+            this.changeFocus(e.target)
+        })
+    }
+}
+
+let myDomManager = new Dom()
---
+++ src/menu.js
@@ -0,0 +1,24 @@
+const todayMenu = {
+    Espresso : 2,
+    Americano : 4,
+    Lungo : 5,
+    Mocha : 4,
+    "Irish Coffee" : 6,
+}
+
+const tomorrowMenu = {
+    Dopio: 4,
+    Lungo : 5,
+    Mocha : 3,
+    "Icelandic Coffee" : 4,
+}
+
+const overmorrowMenu = {
+    Espresso : 2,
+    Ristretto : 4,
+    Latte : 5,
+    Cappucino : 3,
+    "Red Eye" : 4,
+}
+ 
+export {todayMenu, tomorrowMenu, overmorrowMenu}
